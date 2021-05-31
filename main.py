ef51aa04968f2501f567e2b2342423c22e77ac94
AngelD2000 <43766561+AngelD2000@users.noreply.github.com>
2021-05-31 12:12:50 -0600

    Trying to clean up code

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ cur = conn.cursor()
 
 def create():
     cur.execute('''CREATE TABLE IF NOT EXISTS expenses(
-        id real primary key autoincrement, 
-        amount real, 
-        category text, 
-        description text,
-        date_of_expense date
+        ID INTEGER PRIMARY KEY, 
+        amount INTEGER, 
+        category TEXT, 
+        description TEXT,
+        date_of_expense DATE
     )''')
     conn.commit()
 
 
 def commit_spend(amount, category, description=""):
     date_of_expense = date.today()
-    cur.execute("INSERT INTO expenses VALUES (?,?,?,?)", (amount, category, description, date_of_expense))
+    cur.execute("INSERT INTO expenses VALUES (null,?,?,?,?)", (amount, category, description, date_of_expense))
 
 def display_info():
     cur.execute("SELECT * FROM expenses")
@@ -28,7 +28,11 @@ def display_info():
     conn.commit()
 
 def delete_transaction(id):
-    cur.execute("DELETE FROM expenses WHERE id = ?", (id))
+    cur.execute("DELETE FROM expenses WHERE id=?",(id,))
+    conn.commit()
+
+def delete_all():
+    cur.execute("DROP TABLE IF EXISTS expenses")
     conn.commit()
 
 def menu(): 
@@ -37,40 +41,48 @@ def menu():
     print("Type 'create' to reate database")
     print("Type 'commit' to log expense")
     print("Type 'info' to display all expenses")
-    print("Tyep 'modify' to delete logs")
+    print("Type 'delete' to delete logs")
+    print("Type 'drop' to drop the whole table")
     print("Type 'exit' to quit")
+
+def switch(): 
+    user_input = input()
+    if user_input == 'exit':
+        conn.close()
+        print("Database closed successfully")
+        return False
+    elif user_input == 'create':
+        create()
+        print("Log: Database successfully created.\n")
+    elif user_input == 'commit':
+        amount = input("What is the amount you would like to log?\n")
+        if amount.isnumeric():
+            category = input("What is the category of this transaction?\n")
+            description = input("Would you like to add any descriptions?\n")
+            commit_spend(amount, category, description)
+            print("Log: Commit success.\n")
+        else:
+            print("Log: Amount must be a integer")
+            print("Log: Commit failed")
+    elif user_input == 'info':
+        display_info()
+        print("Log: Info display success.\n")
+    elif user_input == 'delete': 
+        id = input("What line would you like to delete?\n")
+        if id.isnumeric(): 
+            delete_transaction(int(id))
+        else:
+            print("Error: Invalid input of id, must be numeric")
+    elif user_input == 'drop':
+        delete_all()
+        print("Exiting...")
+        return False
+    else:
+        print("Error: Invalid option, please choose again.\n")
 
 if __name__ == '__main__':
     flag = True
     while(flag): 
         menu()
-        user_input = input()
-        if user_input == 'exit':
-            flag = False
-            conn.close()
-            print("Database closed successfully")
-        elif user_input == 'create':
-            create()
-            print("Log: Database successfully created.\n")
-        elif user_input == 'commit':
-            amount = input("What is the amount you would like to log?\n")
-            if amount.isnumeric():
-                category = input("What is the category of this transaction?\n")
-                description = input("Would you like to add any descriptions?\n")
-                commit_spend(amount, category, description)
-                print("Log: Commit success.\n")
-            else:
-                print("Log: Amount must be a integer")
-                print("Log: Commit failed")
-        elif user_input == 'info':
-            display_info()
-            print("Log: Info display success.\n")
-        elif user_input == 'modify': 
-            id = input("What line would you like to delete?\n")
-            if id.isnumeric(): 
-                delete_transaction(int(id))
-            else:
-                print("Error: Invalid input of id, must be numeric")
-        else:
-            print("Error: Invalid option, please choose again.\n")
+        flag = switch()
 
