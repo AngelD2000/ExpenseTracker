3479fbace9ed6d6e3e3a11409b7d5807f1c304ef
AngelD2000 <43766561+AngelD2000@users.noreply.github.com>
2021-06-01 23:09:42 -0600

    fixed function before create bugs

--- main.py
+++ main.py
@@ -5,6 +5,51 @@ from datetime import date
 conn = db.connect('track.db')
 cur = conn.cursor()
 
+
+def menu(): 
+    print("--------------")
+    print("Menu.......")
+    print("Type 'create' to reate database")
+    print("Type 'commit' to log expense")
+    print("Type 'info' to display all expenses")
+    print("Type 'delete' to delete logs")
+    print("Type 'drop' to drop the whole table")
+    print("Type 'exit' to quit")
+
+
+def switch(user_input): 
+    if user_input == 'create':
+        create()
+        print("Log: Database successfully created.\n")
+    elif user_input == 'commit':
+        amount = input("What is the amount you would like to log?\n")
+        if amount.isnumeric():
+            category = input("What is the category of this transaction?\n")
+            description = input("Would you like to add any descriptions?\n")
+            commit_spend(amount, category, description)
+            print("Log: Commit success.\n")
+        else:
+            print("Log: Amount must be a integer")
+            print("Log: Commit failed")
+    elif user_input == 'info':
+        display_info()
+        print("Log: Info display success.\n")
+    elif user_input == 'delete': 
+        id = input("What line would you like to delete?\n")
+        if id.isnumeric(): 
+            delete_transaction(int(id))
+        else:
+            print("Error: Invalid input of id, must be numeric")
+    elif user_input == 'drop':
+        delete_all()
+        print("Exiting...")
+    elif user_input == 'exit':
+        conn.close()
+        print("Database closed successfully")
+    else:
+        print("Error: Invalid option, please choose again.\n")
+
+
 def create():
     cur.execute('''CREATE TABLE IF NOT EXISTS expenses(
         ID INTEGER PRIMARY KEY, 
@@ -35,54 +80,20 @@ def delete_all():
     cur.execute("DROP TABLE IF EXISTS expenses")
     conn.commit()
 
-def menu(): 
-    print("--------------")
-    print("Menu.......")
-    print("Type 'create' to reate database")
-    print("Type 'commit' to log expense")
-    print("Type 'info' to display all expenses")
-    print("Type 'delete' to delete logs")
-    print("Type 'drop' to drop the whole table")
-    print("Type 'exit' to quit")
-
-def switch(): 
-    user_input = input()
-    if user_input == 'exit':
-        conn.close()
-        print("Database closed successfully")
-        return False
-    elif user_input == 'create':
-        create()
-        print("Log: Database successfully created.\n")
-    elif user_input == 'commit':
-        amount = input("What is the amount you would like to log?\n")
-        if amount.isnumeric():
-            category = input("What is the category of this transaction?\n")
-            description = input("Would you like to add any descriptions?\n")
-            commit_spend(amount, category, description)
-            print("Log: Commit success.\n")
-        else:
-            print("Log: Amount must be a integer")
-            print("Log: Commit failed")
-    elif user_input == 'info':
-        display_info()
-        print("Log: Info display success.\n")
-    elif user_input == 'delete': 
-        id = input("What line would you like to delete?\n")
-        if id.isnumeric(): 
-            delete_transaction(int(id))
-        else:
-            print("Error: Invalid input of id, must be numeric")
-    elif user_input == 'drop':
-        delete_all()
-        print("Exiting...")
-        return False
-    else:
-        print("Error: Invalid option, please choose again.\n")
-
 if __name__ == '__main__':
     flag = True
+    flag_table = False
     while(flag): 
         menu()
-        flag = switch()
-
+        user_input = input()
+        if user_input == 'exit' or user_input == 'drop':
+            flag = False 
+            switch(user_input)
+        elif user_input == 'create': 
+            flag_table = True
+            switch(user_input)
+        else:
+            if not flag_table: 
+                print("Error: Cannot perform funtion, table has yet been created.")
+            else:
+                switch(user_input)
